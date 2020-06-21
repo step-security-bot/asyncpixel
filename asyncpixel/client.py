5a3adeaaaa17738a55e7536767785b423f53123c
Darkflame72 <leon@MacBook-Pro.localdomain>
2020-06-21 12:14:23 +1200

    improved docstrings

--- asyncpixel/client.py
+++ asyncpixel/client.py
@@ -15,7 +15,11 @@ class Client:
     """client class for hypixel wrapper"""
 
     def __init__(self, api_keys: Union[list, str]):
-        """basic initialization of the hypixel API client"""
+        """Initialise base class by storing keys and creating session
+
+        Args:
+            api_keys (Union[list, str]): list or signle api key to use
+        """
 
         # Handles the instance of a singular key
         if not isinstance(api_keys, list):
@@ -31,11 +35,24 @@ class Client:
         """used for safe client cleanup and stuff"""
         await self.session.close()
 
-    async def get(self, url: str, params: dict = {}) -> dict:
-        """base method to fetch a response from hypixel"""
+    async def get(self, path: str, params: dict = {}) -> dict:
+        """Base function to get raw data from hypixel
+
+        Args:
+            path (str): path that you wish to request from
+            params (dict, optional): parameters to pass into request. Defaults to {}.
+
+        Raises:
+            RateLimitError: error if ratelimit has been reached
+
+        Returns:
+            dict: returns a dictionary of the json response
+        """
         params["key"] = choice(self.api_keys)
 
-        response = await self.session.get(f"{self.base_url}{url}", params=params)
+        response = await self.session.get(
+            f"{self.base_url}{path}", params=params,
+        )
 
         if response.status == 429:
             raise RateLimitError("Hypixel")
@@ -43,8 +60,14 @@ class Client:
         return await response.json()
 
     async def KeyData(self, key: str = None) -> Key:
-        """fetches information from the api about the key used
-        uses a key assinged to the class if not provided"""
+        """Get information about an api key.
+
+        Args:
+            key (str, optional): api key. Defaults tokey provided in class.
+
+        Returns:
+            Key: Key object
+        """
 
         if key is None:
             key = choice(self.api_keys)
@@ -61,7 +84,11 @@ class Client:
         )
 
     async def WatchdogStats(self) -> WatchDog:
-        """Get stats around bans."""
+        """Get current watchdog stats.
+
+        Returns:
+            WatchDog: WatchDog stats object
+        """
         data = await self.get("watchdogstats")
 
         return WatchDog(
@@ -73,7 +100,14 @@ class Client:
         )
 
     async def PlayerStatus(self, uuid: str) -> Status:
-        """Get current online status about a player"""
+        """Get current online status about a player.
+
+        Args:
+            uuid (str): uuid of player
+
+        Returns:
+            Status: Status object of player
+        """
 
         data = await self.get("status", params={"uuid": uuid})
         if data["session"]["online"]:
