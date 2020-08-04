4f6545cdc44ecdf10186b5234c1ee550d4914755
Darkflame72 <31436575+Darkflame72@users.noreply.github.com>
2020-08-05 07:57:32 +1200

    fix naming error

--- asyncpixel/models/status.py
+++ asyncpixel/models/status.py
@@ -1,6 +1,6 @@
 class Status:
-    def __init__(self, online: bool, gameType=None, _mode=None, map=None):
+    def __init__(self, online: bool, gameType=None, _mode=None, _map=None):
         self.online = online
         self.gameType = gameType
-        self.mode = mode
+        self.mode = _mode
         self.map = _map
