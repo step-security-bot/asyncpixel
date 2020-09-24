525f3b601ba2425f37978f96a20a6db5a9d7d426
Darkflame72 <31436575+Darkflame72@users.noreply.github.com>
2020-09-24 12:05:02 +1200

    fix flake8 warnings in exceptions

--- asyncpixel/exceptions/exceptions.py
+++ asyncpixel/exceptions/exceptions.py
@@ -1,31 +1,56 @@
-class RateLimitError(Exception):
-    """Raised when a ratelimit is reached"""
+"""All exceptions for asyncpixel."""
 
-    def __init__(self, source="unknown source"):
+
+class RateLimitError(Exception):
+    """Raised when a ratelimit is reached."""
+
+    def __init__(self, source: str = "unknown source") -> None:
+        """Error raised when ratelimit reached.
+
+        Args:
+            source (str, optional): Source of the error. Defaults to "unknown source".
+        """
         self.message = f"The {source}API ratelimit was reached!"
         super().__init__(self.message)
 
-    def __str__(self):
+    def __str__(self) -> str:
+        """Return error in readable format.
+
+        Returns:
+            str: string version of error
+        """
         return self.message
 
 
 class ApiNoSuccess(Exception):
-    """Raised when an error occurred"""
+    """Raised when an error occurred."""
 
-    def __init__(self):
+    def __init__(self) -> None:
+        """Create error."""
         self.message = "The method encountered an error on the hypixel side."
         super().__init__(self.message)
 
-    def __str__(self):
+    def __str__(self) -> str:
+        """Return error in readable format.
+
+        Returns:
+            str: string version of error
+        """
         return self.message
 
 
 class InvalidApiKey(Exception):
-    """Raised when api key is incorrect"""
+    """Raised when api key is incorrect."""
 
-    def __init__(self):
+    def __init__(self) -> None:
+        """Create error."""
         self.message = "Entered API key is not valid"
         super().__init__(self.message)
 
-    def __str__(self):
+    def __str__(self) -> str:
+        """Return error in readable format.
+
+        Returns:
+            str: string version of error
+        """
         return self.message
