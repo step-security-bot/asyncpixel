b007a5a5225e188dcb9b5d338f93421d5b6c6721
Darkflame72 <31436575+Darkflame72@users.noreply.github.com>
2020-09-30 17:34:24 +1300

    update __init__.py

--- asyncpixel/__init__.py
+++ asyncpixel/__init__.py
@@ -8,9 +8,16 @@ try:
 except ImportError:  # pragma: no cover
     from importlib_metadata import version, PackageNotFoundError  # type: ignore
 
+from .client import Client
 
 try:
     __version__ = version(__name__)
 except PackageNotFoundError:  # pragma: no cover
     __version__ = "unknown"
 __author__ = "Leon Bowie"
+
+__all__ = [
+    "__version__",
+    "__author__",
+    "Client",
+]
