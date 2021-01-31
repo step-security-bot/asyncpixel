fba6341a4ccdfc5406734fa01c23ee262e95ba63
Darkflame72 <31436575+Darkflame72@users.noreply.github.com>
2021-01-31 13:05:27 +1300

    update sphinx version

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ author = "Leon Bowie"
 copyright = f"{datetime.now().year}, {author}"
 
 # The full version, including alpha/beta/rc tags
-release = "1.0.0"
+release = "1.0.3"
 
 
 # -- General configuration ---------------------------------------------------
