3cf7bf4bf97cdf30aaf7cf8a6b564f936e9cdda0
Leon Bowie <31436575+Darkflame72@users.noreply.github.com>
2021-02-10 09:24:05 +1300

    Version 1.0.4 (#109)
    * Version 1.0.4
    
    * Update pyproject.toml

--- tests/test_asyncpixel.py
+++ tests/test_asyncpixel.py
@@ -8,7 +8,7 @@ from .utils import generate_key
 
 def test_version() -> None:
     """Mock version."""
-    assert asyncpixel.__version__ == "1.0.3"
+    assert asyncpixel.__version__ == "1.0.4"
 
 
 def test_author() -> None:
