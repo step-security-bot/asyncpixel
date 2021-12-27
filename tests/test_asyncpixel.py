a2f0c7616e4b13a3b5ac51f180a61f98de500779
Leon Bowie <leon@bowie-co.nz>
2021-12-27 20:26:17 +1300

    Release v1.2.1

--- tests/test_asyncpixel.py
+++ tests/test_asyncpixel.py
@@ -10,7 +10,7 @@ from .utils import generate_key
 
 def test_version() -> None:
     """Mock version."""
-    assert asyncpixel.__version__ == "1.2.0"
+    assert asyncpixel.__version__ == "1.2.1"
 
 
 def test_author() -> None:
