4639cd0d52a8487ac4218b270a84ba8a428f967f
Leon Bowie <31436575+Darkflame72@users.noreply.github.com>
2022-03-01 22:32:24 +1300

    Version 1.3.1 (#483)
    * Update pyproject.toml
    
    * Update test_asyncpixel.py
    
    * Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ author = "Leon Bowie"
 copyright = f"{datetime.now().year}, {author}"
 
 # The full version, including alpha/beta/rc tags
-release = "1.3.0"
+release = "1.3.1"
 
 
 # -- General configuration ---------------------------------------------------
