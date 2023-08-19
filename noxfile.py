a66d781a30cb02d88f392a21e269540138c64f95
Leon Bowie <leon@bowie-co.nz>
2023-08-19 17:07:10 +1200

    ci: remove safety check (#805)

--- noxfile.py
+++ noxfile.py
@@ -13,7 +13,6 @@ package = "asyncpixel"
 python_versions = ["3.11", "3.10", "3.9", "3.8"]
 nox.options.sessions = (
     "pre-commit",
-    "safety",
     "mypy",
     "tests",
     "docs-build",
@@ -90,19 +89,6 @@ def precommit(session: Session) -> None:
     session.run("pre-commit", *args)
     if args and args[0] == "install":
         activate_virtualenv_in_precommit_hooks(session)
-
-
-@session(python=python_versions[0])
-def safety(session: Session) -> None:
-    """Scan dependencies for insecure packages."""
-    requirements = session.poetry.export_requirements()
-    session.install("safety")
-    session.run(
-        "safety",
-        "check",
-        f"--file={requirements}",
-        "--bare",
-    )
 
 
 @session(python=python_versions)
